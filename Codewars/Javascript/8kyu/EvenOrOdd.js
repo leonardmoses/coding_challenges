f710a83e904b2b56e50faef23c97e9ac4f5f8045
Qalexeon <qalexeon@Xavier.home>
2023-05-02 17:04:47 -0400

    update Render page

---
+++ Codewars/Javascript/8kyu/EvenOrOdd.js
@@ -0,0 +1,14 @@
+const ChallengeName = 'EvenOrOdd'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+function evenOrOdd(number) {
+  
+}
+
+
+
