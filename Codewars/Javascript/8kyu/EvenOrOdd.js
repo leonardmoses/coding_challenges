6bdcbbd00eab2cad90bc08835ac0ab6d22fd116e
Qalexeon <qalexeon@Xavier.home>
2023-05-03 19:02:59 -0400

    EvenOrOdd complete

--- Codewars/Javascript/8kyu/EvenOrOdd.js
+++ Codewars/Javascript/8kyu/EvenOrOdd.js
@@ -6,9 +6,17 @@ title.innerHTML= ChallengeName;
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 
+
 function evenOrOdd(number) {
-  
+   console.log(number)
+   if (number%2 == 0) {
+    console.log("even")
+   } else {
+    console.log("odd")
+   }
+
+//    return number%2 == 0 ? "even" : "odd"
 }
 
 
-
+evenOrOdd(6)
