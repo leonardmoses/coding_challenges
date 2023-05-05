763011ce99aa849af11f80df3438e9a48303f0d9
Qalexeon <leonardsamuelmoses@gmail.com>
2023-05-05 17:13:39 -0400

    OppositeNumber completed

---
+++ Codewars/Javascript/8kyu/OppositeNumber.js
@@ -0,0 +1,19 @@
+const ChallengeName = 'OppositeNumber'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+
+
+function opposite(number) {
+    console.log(number * -1)
+
+    // return number * -1
+  }
+
+
+  opposite(1)
+  opposite(14)
+  opposite(-34)
