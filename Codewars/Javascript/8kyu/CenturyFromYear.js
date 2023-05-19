809855b27482151188cd89770dcdd775c6ab157f
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-05-19 14:58:00 -0400

    CenturyFromYear Completed. Edge case solved. Did not need to use conditional, instead used math ceiling.

--- Codewars/Javascript/8kyu/CenturyFromYear.js
+++ Codewars/Javascript/8kyu/CenturyFromYear.js
@@ -10,8 +10,18 @@ Given a year, return the century it is in.
 */
 
 function century(year) {
-
-    console.log( year%2===0 ? Math.floor(year/100) : Math.floor(year/100) +1 )
+    console.log(year)
+    console.log( Math.ceil(year/100) )
+    
+    // console.log( year%2 > 0 ? Math.floor(year/100 + 1)  : Math.floor(year/100)  )
+    
+    // if (year%2===0) {
+    //     console.log(Math.floor(year/100))
+    // } else if (year=) {
+        
+    // } else {
+    //     console.log( (Math.floor(year/100)) + 1)
+    // }
 
   }
 
@@ -26,5 +36,7 @@ century(2000)
 // 20
 century(89)
 // 1
+century(405102)
+// 4052
 
 //Edge Case: Testing for year 405102: expected 4051 to equal 4052
