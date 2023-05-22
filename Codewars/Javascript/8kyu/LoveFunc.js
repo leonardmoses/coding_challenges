e38524a28d25c34226081b12553a58e45617617e
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-05-22 13:28:51 -0400

    LoveFunc completed

---
+++ Codewars/Javascript/8kyu/LoveFunc.js
@@ -0,0 +1,24 @@
+const ChallengeName = 'LoveFunc'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+function lovefunc(flower1, flower2){
+    console.log( (flower1%2 == 0 && flower2 %2 != 0) || (flower2%2 == 0 && flower1%2 != 0) ? true : false )
+  }
+
+
+lovefunc(1,4)
+//   , true)
+lovefunc(2,2)
+//   , false)
+lovefunc(0,1)
+//   , true)
+lovefunc(0,0)
+//   , false)
