6d81c81a064fb25ef1093819667b71ff3a4c53a2
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-05-23 13:50:31 -0400

    SumArrays completed using reduce method.

--- Codewars/Javascript/8kyu/SumArrays.js
+++ Codewars/Javascript/8kyu/SumArrays.js
@@ -7,10 +7,14 @@ title.innerHTML= ChallengeName;
 Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 */
 
+// function sum (numbers) {
+//     let sum = 0
+//     numbers.map(i => sum += i)
+//     console.log(sum)
+// };
+
 function sum (numbers) {
-    let sum = 0
-    numbers.map(i => sum += i)
-    console.log(sum)
+    console.log( numbers.length === 0 ? 0 : numbers.reduce( (a,b) => a+b ))
 };
   
 
@@ -19,3 +23,7 @@ sum([])
 // , 0);
 sum([1, 5.2, 4, 0, -1])
 // , 9.2);
+sum([1, 10, 4, 4, -1])
+// , 18);
+sum([1, 4, 9, 0, -9])
+// , 5);
