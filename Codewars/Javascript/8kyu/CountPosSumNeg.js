c1fcf6f044e15d54bef187956739efda1b9799b4
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-05 19:04:19 -0400

    CountPosSumNeg reworked completed. Accounted for emtpy array and null edge cases.

--- Codewars/Javascript/8kyu/CountPosSumNeg.js
+++ Codewars/Javascript/8kyu/CountPosSumNeg.js
@@ -15,21 +15,26 @@ function countPositivesSumNegatives(input) {
     const arrayPos = []
     const arrayNeg = []
     let sumNeg = 0
+    let answer = []
 
-    input.forEach(element => {
-        if(element >= 0) {
-            arrayPos.push(element)
-        } else {
-            arrayNeg.push(element)
-        }
-    });
-
-    arrayNeg.map(x => sumNeg += x)
-    const answer = [arrayPos.length, sumNeg]
-
-    console.log(answer)
+    if (input == null || input.length == 0 ) {
+        console.log(answer)
+    } else {
+        input.forEach(element => {
+            if (element >= 0 ) {
+                arrayPos.push(element)
+            } else {
+                arrayNeg.push(element)
+            }
+        });
+        arrayNeg.map(x => sumNeg += x)
+        answer = [arrayPos.length, sumNeg]
+        console.log(answer)
+    }
   }
 
 
 countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
 countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])
+countPositivesSumNegatives([])
+countPositivesSumNegatives(null)
