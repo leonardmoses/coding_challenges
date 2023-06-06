1719a159750e826a6e0aed9c0e7e936160e51e7f
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-06 15:09:12 -0400

    CountByX Completed

---
+++ Codewars/Javascript/8kyu/CountByX.js
@@ -0,0 +1,27 @@
+const ChallengeName = 'CountByX'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+*/
+
+function countBy(x, n) {
+    let z = [];
+    // z.push(x)
+    for (let i = 1; i<=n; i++) {
+        z.push(x*i)
+    }
+    console.log(z)
+
+  }
+
+countBy(1,10)
+// , [1,2,3,4,5,6,7,8,9,10], "Array does not match")
+countBy(2,5)
+// , [2,4,6,8,10], "Array does not match")
