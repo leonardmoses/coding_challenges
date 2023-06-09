bf7be249adc92c0773a40ed6eac6a32e51934949
Qalexeon <leonardsamuelmoses@gmail.com>
2023-06-08 21:23:07 -0400

    ArrayPlusArray refactored using concat and reduce to make a 1 line solution.

--- Codewars/Javascript/8kyu/ArrayPlusArray.js
+++ Codewars/Javascript/8kyu/ArrayPlusArray.js
@@ -10,12 +10,12 @@ P.S. Each array includes only integer numbers. Output is a number too.
 */
 
 function arrayPlusArray(arr1, arr2) {
-    let answer = 0
-    const arr3 = arr1.concat(arr2).forEach(x => {answer += x})
-    console.log(answer)
+
+    console.log( arr1.concat(arr2).reduce((x , initial) => x + initial) )
+
   }
 
-
+ 
 arrayPlusArray([1, 2, 3], [4, 5, 6])
 // , 21);
 arrayPlusArray([-1, -2, -3], [-4, -5, -6])
