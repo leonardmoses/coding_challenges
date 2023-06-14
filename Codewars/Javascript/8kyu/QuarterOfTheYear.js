a63300bf3593ddd96dc9987940065b7a11f421d4
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-14 15:20:53 -0400

    QuarterOfTheYear Completed.

---
+++ Codewars/Javascript/8kyu/QuarterOfTheYear.js
@@ -0,0 +1,27 @@
+const ChallengeName = 'QuarterOfTheYear'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+1 <= month <= 12
+*/
+
+
+const quarterOf = (month) => {
+    console.log( month<=3 ? 1 : month<=6 ? 2 : month<=9 ? 3 : 4)
+  }
+ 
+
+quarterOf(3)
+// , 1)
+quarterOf(8)
+// , 3)
+quarterOf(11)
+// , 4)
