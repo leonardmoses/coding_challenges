1338c1b464f2e9cc5fb5c14863eb1ec3bdebc8fe
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-16 15:52:12 -0400

    SwitchItUp Completed.

---
+++ Codewars/Javascript/8kyu/SwitchItUp.js
@@ -0,0 +1,69 @@
+const ChallengeName = 'SwitchItUp'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+*/
+
+
+function switchItUp(number){
+    let result;
+    switch(number) {
+        case 1: {
+            result = 'One'
+            break
+        }
+        case 2: {
+            result = 'Two'
+            break
+        }
+        case 3: {
+            result = 'Three'
+            break
+        }
+        case 4: {
+            result = 'Four'
+            break
+        }
+        case 5: {
+            result = 'Five'
+            break
+        }
+        case 6: {
+            result = 'Six'
+            break
+        }
+        case 7: {
+            result = 'Seven'
+            break
+        }
+        case 8: {
+            result = 'Eight'
+            break
+        }
+        case 9: {
+            result = 'Nine'
+            break
+        }
+        case 0: {
+            result = 'Zero'
+        }
+    }
+    console.log(result)
+}
+
+
+switchItUp(1)
+// ,"One");
+switchItUp(3)
+// ,"Three");
+switchItUp(5)
+// ,"Five");
