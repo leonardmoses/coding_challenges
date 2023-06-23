2f7029b0a7602ce9618390528d05b840e4fee118
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-23 15:16:04 -0400

    MessiGoals Completed.

---
+++ Codewars/Javascript/8kyu/MessiGoals.js
@@ -0,0 +1,35 @@
+const ChallengeName = 'MessiGoals'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    console.log(laLigaGoals+copaDelReyGoals+championsLeagueGoals)
+  }
+
+
+
+
+goals(0,0,0)
+// , 0)
+goals(43, 10, 5)
+// , 58)
+
+
+
