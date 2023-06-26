eec1cb67ed67e3e619567822f0e18e0326b1b345
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-26 13:51:10 -0400

    SortAndStar Completed.

---
+++ Codewars/Javascript/8kyu/SortAndStar.js
@@ -0,0 +1,26 @@
+const ChallengeName = 'SortAndStar'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+*/
+
+
+function twoSort(s) {
+
+    console.log(s.sort()[0].split('').join('***'))
+    
+}
+
+
+
+twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])
+// , 'b***i***t***c***o***i***n' );
+twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"])
+// , 'a***r***e');
