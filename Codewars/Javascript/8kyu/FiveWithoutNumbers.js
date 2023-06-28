fb251ba3560a34edc4e93433090e19b410d37da2
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-06-28 15:32:49 -0400

    FiveWithoutNumbers Completed.

---
+++ Codewars/Javascript/8kyu/FiveWithoutNumbers.js
@@ -0,0 +1,20 @@
+const ChallengeName = 'FiveWithoutNumbers'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+
+function unusualFive() {
+    console.log( ['a','b','c','d','e'].length )
+  }
+
+
+unusualFive()
