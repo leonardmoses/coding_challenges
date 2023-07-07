db6b3026e91e6ecb3d17e3893b10c119fd65abe3
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-07-07 14:39:59 -0400

    WideMouthFrog Completed.

---
+++ Codewars/Javascript/8kyu/WideMouthFrog.js
@@ -0,0 +1,28 @@
+const ChallengeName = 'WideMouthFrog'
+let title = document.querySelector('#title');
+title.innerHTML= ChallengeName;
+
+
+/* 
+The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+*/
+
+
+function mouthSize(animal) {
+    console.log(animal.toLowerCase() == 'alligator' ? 'small' : 'wide')
+  }
+
+
+
+mouthSize("toucan")
+// ,"wide")
+mouthSize("ant bear")
+// ,"wide")
+mouthSize("alligator")
+// ,"small")
