b17e7e49b0a4af4530d15683022b128b1c04f925
Leonard Moses <leonardsamuelmoses@gmail.com>
2023-07-13 13:03:16 -0400

    GenerateRangeOfIntegers Completed.

--- Codewars/Javascript/7kyu/Isograms.js
+++ Codewars/Javascript/7kyu/Isograms.js
@@ -1,7 +1,6 @@
-const ChallengeName = 'Isograms'
-let title = document.querySelector('#title');
-title.innerHTML= ChallengeName;
-
+const ChallengeName = "Isograms";
+let title = document.querySelector("#title");
+title.innerHTML = ChallengeName;
 
 /* 
 An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
@@ -15,28 +14,23 @@ isIsogram "moose" = false
 isIsogram "aba" = false
 */
 
+function isIsogram(str) {
+  console.log(str);
+  const array1 = str.toLowerCase().split("");
+  console.log(array1);
 
-function isIsogram(str){
-    console.log(str)
-    const array1 = str.toLowerCase().split('')
-    console.log(array1)
-
-    for (let i=0; i<array1.length; i++) {
-        console.log('-------------->'+array1[i])
-        for (let j=0; j<array1.length; j++) {
-            // console.log(array1[j])
-            if (array1[j]===array1[i]) {
-                console.log(true)
-            } else {
-                console.log(false)
-            }
-        }
+  for (let i = 0; i < array1.length; i++) {
+    console.log("-------------->" + array1[i]);
+    for (let j = 0; j < array1.length; j++) {
+      // console.log(array1[j])
+      if (array1[j] === array1[i]) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
     }
-
-
   }
-
-
+}
 
 // isIsogram("Dermatoglyphics")
 // // , true );
@@ -44,7 +38,7 @@ function isIsogram(str){
 // // , true );
 // isIsogram("aba")
 // , false, "same chars may not be adjacent" );
-isIsogram("moOse")
+isIsogram("moOse");
 // , false, "same chars may not be same case" );
 // isIsogram("isIsogram")
 // // , false );
